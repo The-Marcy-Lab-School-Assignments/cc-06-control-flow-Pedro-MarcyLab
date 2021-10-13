b8f660945201d49e9441f84220ab2f628a235a7d
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-10-13 13:59:33 +0000

    question 1 complete

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //1
 function sumOfThreeAndFive(){
+    sum = 0
+    for(let i = 1; i < 1000; i++){
+        if(i % 3 === 0 || i % 5 ===0){
+            sum += i;
+        }
+        
+    }
+    return sum
+
+   
   
 }
 
-function greaterNum(){
-  
+function greaterNum(x,y){
+  if(x > y){
+      return x
+  } 
+  else if (y > x){
+     return y
+    } else{
+      return "both integers are equal"
+  }
 }
+console.log(greaterNum(6,9))
 
 function oddAndEvenInFifteen(){
   
@@ -17,6 +35,6 @@ function sortThreeNums(){
 
 
 
-
+ console.log(sumOfThreeAndFive())
 
 
